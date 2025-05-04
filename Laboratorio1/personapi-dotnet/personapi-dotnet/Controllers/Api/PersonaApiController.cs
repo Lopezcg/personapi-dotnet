b2a1f9c4229bb22b2dabecc859ed5d249bb12c82
Carlos Lopez <carlosgablopezm@gmail.com>
2025-05-04 18:45:34 -0500

    feat: persona working

---
+++ Laboratorio1/personapi-dotnet/personapi-dotnet/Controllers/Api/PersonaApiController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using personapi_dotnet.Models.Entities;
+using personapi_dotnet.Services.Interfaces;
+
+namespace personapi_dotnet.Controllers.Api
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PersonaApiController : ControllerBase
+    {
+        private readonly IPersonaService _personaService;
+
+        public PersonaApiController(IPersonaService personaService)
+        {
+            _personaService = personaService;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_personaService.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var persona = _personaService.GetById(id);
+            if (persona == null)
+                return NotFound();
+
+            return Ok(persona);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Persona persona)
+        {
+            _personaService.Add(persona);
+            return CreatedAtAction(nameof(GetById), new { id = persona.Cc }, persona);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, [FromBody] Persona persona)
+        {
+            if (id != persona.Cc)
+                return BadRequest();
+
+            _personaService.Update(persona);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _personaService.Delete(id);
+            return NoContent();
+        }
+    }
+}
--- Laboratorio1/personapi-dotnet/personapi-dotnet/Program.cs
+++ Laboratorio1/personapi-dotnet/personapi-dotnet/Program.cs
@@ -1,7 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using personapi_dotnet.Models.Entities;
+using personapi_dotnet.Services.Implementations;
+using personapi_dotnet.Services.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
+// Registro del DbContext
+builder.Services.AddDbContext<persona_dbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//  Registro del DAO (PersonaService)
+builder.Services.AddScoped<IPersonaService, PersonaService>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
@@ -9,6 +24,11 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
+}
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 app.UseStaticFiles();
 
